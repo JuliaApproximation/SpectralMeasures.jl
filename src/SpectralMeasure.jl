6bdae5ac4ae504764da91f05462c5e82bd5ecec9
Marcus <marcusdavidwebb@gmail.com>
2015-06-04 16:26:44 +0100

    Added a conversionLmatrix(a,b,c,d,N) function. This computes the change of basis operator from the polynomials with jacobioperator(a,b) to the polynomials with jacobioperator(c,d). It's still based on a linear recurrence relation. The dependencies are still in an "arrow head shape".
    Changed the spectralmeasures function so that if we only have continuous spectrum then we get a Chebyshev T series (weighted by (1-x^2)^-.5). It's a little slow at the moment because we're doing this in a really inefficient manner: compute the L matrix from Chebyshev T to jacobioperator(a,b) for FULL SIZE 10,000 by 10,000. This is stupid but will work for the moment while we work out the most efficient way to compute the entries of L. It appears to work as hoped i.e. the number of coefficients in the expansion are very few when we nearly have singularities at the end points. And when we go for the semicircle, we simple get a quadratic (1-x^2).

--- src/SpectralMeasure.jl
+++ src/SpectralMeasure.jl
@@ -12,21 +12,29 @@ function spectralmeasure(a,b;maxlength::Int=10000)
     # b is the first n-1 off-diagonal elements of J (.5 thereafter)
 
     # Finds T,K such that L = T+K, where L takes LJL^{-1} = Toeplitz([0,1/2])
+    # This is purely for determining discrete eigenvalues
     T,K=tkoperators(a,b)
-
-    # We still have no idea why this finds the discrete eigenvalues of L
     Tfun = Fun([T[1,1];T.negative],Taylor)
     eigs=sort!(real(map!(joukowsky,filter!(z->abs(z)<1 && isreal(z),complexroots(Tfun)))))
 
-    # If there are no discrete eigenvalues, produce the measure using the L we already have
     if isempty(eigs)
-        L=T+K
-        coeffs = L\[1]
-        Fun((2/π)*coeffs,JacobiWeight(.5,.5,Ultraspherical{1}()))
+      #####
+      # Old style that just used the L matrix we already have from above
+      #L=T+K
+      #coeffs = L\[1]
+      #Fun((2/π)*coeffs,JacobiWeight(.5,.5,Ultraspherical{1}()))
+      #####
+
+      #THIS IS REALLY INEFFICIENT. I'M WORKING ON A FAST VERSION (MW)
+      L = conversionLmatrix([0,0,0],[1/sqrt(2),.5],a,b,maxlength)
+      coeffs = chop(L[1:maxlength,1],10eps())
+      Fun((1/π)*[coeffs[1];sqrt(2)*coeffs[2:end]],JacobiWeight(-.5,-.5,Ultraspherical{0}()))
+
     # If there are discrete eigenvalues then we must deflate using QL iteration
     else
+        numeigs = length(eigs)
         Q=Array(BandedOperator{Float64},0)
-        for k=1:length(eigs)
+        for k=1:numeigs
             t1,t0=0.5,eigs[k]
             # TODO replace this 1-step QL iteration with iteration until LQ[1,2]<ϵ
             Q1,L1=ql(a-t0,b,-t0,t1)
@@ -42,25 +50,24 @@ function spectralmeasure(a,b;maxlength::Int=10000)
             eigs[k]=LQ[1,1]+t0
         end
 
-        # note that a and b have been changed after deflation
+        # note that a and b have been changed (within this function call) after deflation
         T,K = tkoperators(a,b)
         L2 = T+K
 
         q0=[1.0]
-        m=length(Q) #number of discrete eigenvalues
-        for k=1:m
+        for k=1:numeigs
             q0=[q0[1:k-1];Q[k]'*q0[k:end]]
         end
-        coeffs=linsolve(L2,q0[m+1:end];maxlength=maxlength)
-        c=q0[m+1]
-        Fun([q0[1:m].^2;(2c/π)*coeffs],DiracSpace(JacobiWeight(0.5,0.5,Ultraspherical{1}()),eigs))
+        coeffs=linsolve(L2,q0[numeigs+1:end];maxlength=maxlength)
+        c=q0[numeigs+1]
+        Fun([q0[1:numeigs].^2;(2c/π)*coeffs],DiracSpace(JacobiWeight(0.5,0.5,Ultraspherical{1}()),eigs))
     end
 end
 
 function tkoperators(a,b)
     @assert length(a)-length(b)==1
     n = length(a)
-    L = Lmatrix(a,b,2n)
+    L = conversionLmatrix(a,b,2n)
 
     T=ToeplitzOperator(vec(L[2*n,2*n-1:-1:1]),[L[2*n,2*n]])
     K = zeros(2*n,2*n)
@@ -73,56 +80,50 @@ function tkoperators(a,b)
     T,K
 end
 
-# This is for Chebyshev U
-function Lmatrix(a,b,N)
-    n = length(a)
-    @assert n-length(b)==1
-    bext = [b; .5]
-    L = zeros(N,N)
-    L[1,1] = 1
-    L[2,1] = -a[1]/bext[1]
-    L[2,2] = 0.5/bext[1]
+function conversionLmatrix(a,b,c,d,N)
+  @assert N >= 2*max(length(a),length(b),length(c),length(d))
+  a = [a;zeros(N-length(a))]; b = [b;.5+zeros(N-length(b))]
+  c = [c;zeros(N-length(c))]; d = [d;.5+zeros(N-length(d))]
 
-    # the generic case.
-    for i = 3:n+1
-        L[i,1] = (L[i-1,2]/2-a[i-1]*L[i-1,1]-bext[i-2]*L[i-2,1])/bext[i-1]
-        for j = 2:i
-            L[i,j] = (L[i-1,j+1]/2+L[i-1,j-1]/2-a[i-1]*L[i-1,j]-bext[i-2]*L[i-2,j])/bext[i-1]
-        end
+  L = zeros(N,N)
+  L[1,1] = 1
+  L[2,1] = (c[1]-a[1])/b[1]
+  L[2,2] = d[1]/b[1]
+  for i = 3:N
+    L[i,1] = ((c[1]-a[i-1])*L[i-1,1] + d[1]*L[i-1,2] - b[i-2]*L[i-2,1])/b[i-1]
+    for j = 2:i-1
+      L[i,j] = (d[j-1]*L[i-1,j-1] + (c[j]-a[i-1])*L[i-1,j] + d[j]*L[i-1,j+1] - b[i-2]*L[i-2,j])/b[i-1]
     end
-    # this case is where b[m],b[m-1],b[m-2] = 1/2, and a[m],a[m-1] = 0, like Chebyshev
-    for m = n+2:N
-        L[m,1] = L[m-1,2]-L[m-2,1]
-        for j = 2:m-1
-            L[m,j] = L[m-1,j+1]-L[m-2,j]+L[m-1,j-1]
-        end
-        L[m,m] = -L[m-2,m] + L[m-1,m-1]
-    end
-    L
+    L[i,i] = d[i-1]*L[i-1,i-1]/b[i-1]
+  end
+  L
 end
+
+# This is for Chebyshev U
+conversionLmatrix(a,b,N) = conversionLmatrix(a,b,[],[],N)
 
 function jacobimatrix(a,b,t0,t1,N)
     J = zeros(N,N)
     J[1,1]=a[1]
-    n=length(a)
-    for i = 1:n-1
+    a = [a;t0*ones(N-length(a))]
+    b = [b;t1*ones(N-length(b))]
+    for i = 1:N-1
         J[i+1,i+1] = a[i+1]
         J[i,i+1] = b[i]
         J[i+1,i] = b[i]
     end
-    for i = n:N-1
-        J[i,i+1] = t1
-        J[i+1,i+1] = t0
-        J[i+1,i] = t1
-    end
     J
 end
 
+jacobimatrix(a,b,N) = jacobimatrix(a,b,0,.5,N)
+
 function jacobioperator(a,b,t0,t1)
     Δ=ToeplitzOperator([t1],[t0,t1])
-    B=jacobimatrix(a-t0,b-t1,t0,t1,length(a))
+    B=jacobimatrix(a-t0,b-t1,t0,t1,max(length(a),length(b)+1))
     Δ+CompactOperator(B)
 end
+
+jacobioperator(a,b) = jacobioperator(a,b,0,.5)
 
 immutable ToeplitzGivens <: BandedOperator{Float64}
     c::Float64
@@ -228,39 +229,5 @@ function ql(a,b,t0,t1)
 
     partialgivens(TQ,n+1)*(I+CompactOperator(Q)),TL+CompactOperator(L)
 end
-
-# This is for Chebyshev T
-function Lmatrix2(a,b,N)
-    n = length(a)
-    @assert n-length(b)==1
-    bext = [b; .5]
-    L = zeros(N,N)
-    L[1,1] = 1
-    L[2,1] = -a[1]/bext[1]
-
-    L[2,2] = 1/(sqrt(2)*bext[1])
-
-    # the generic case.
-    for i = 3:n+1
-        L[i,1] = (L[i-1,2]/sqrt(2)-a[i-1]*L[i-1,1]-bext[i-2]*L[i-2,1])/bext[i-1]
-        L[i,2] = (L[i-1,3]/2+L[i-1,1]/sqrt(2)-a[i-1]*L[i-1,2]-bext[i-2]*L[i-2,2])/bext[i-1]
-
-        for j = 3:i
-            L[i,j] = (L[i-1,j+1]/2+L[i-1,j-1]/2-a[i-1]*L[i-1,j]-bext[i-2]*L[i-2,j])/bext[i-1]
-        end
-    end
-    # this case is where b[m],b[m-1],b[m-2] = 1/2, and a[m],a[m-1] = 0, like Chebyshev
-    for m = n+2:N
-        L[m,1] = L[m-1,2]*sqrt(2)-L[m-2,1]
-        L[m,2] = L[m-1,3]+L[m-1,1]*sqrt(2)-L[m-2,2]
-        for j = 3:m-1
-            L[m,j] = L[m-1,j+1]-L[m-2,j]+L[m-1,j-1]
-        end
-        L[m,m] = -L[m-2,m] + L[m-1,m-1]
-    end
-    L
-end
-
-
 
 end  #Module
