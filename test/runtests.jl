7cce2e284ddb4304594b0a5a36ad9e3da903f39d
Sheehan Olver <solver@mac.com>
2015-12-26 19:31:57 +1100

    evaluate -> function call and fix concatenate warnings

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,33 +9,33 @@ using SpectralMeasures
 a = []; b=[]
   μ=spectralmeasureU(a,b)
   ν=spectralmeasureT(a,b)
-  maximum(evaluate(μ,[-.99:.01:.99])-evaluate(ν,[-.99:.01:.99]))
+  maximum(μ(-.99:.01:.99)-ν(-.99:.01:.99))
 
 # Chebyshev T
 a = [0.]; b=[1/sqrt(2),.5]
   μ=spectralmeasureU(a,b,maxlength=100000)
   ν=spectralmeasureT(a,b)
-  maximum(evaluate(μ,[-.99:.01:.99])-evaluate(ν,[-.99:.01:.99]))
+  maximum(μ(-.99:.01:.99)-ν(-.99:.01:.99))
 
 # Legendre
-n=100;a=zeros(n); b=[1:n-1]./sqrt(4*[1:n-1].^2-1)
+n=100;a=zeros(n); b=(1:n-1)./sqrt(4*(1:n-1).^2-1)
   μ=spectralmeasureU(a,b)
   ν=spectralmeasureT(a,b)
-  maximum(evaluate(μ,[-.99:.01:.99])-evaluate(ν,[-.99:.01:.99]))
+  maximum(μ(-.99:.01:.99)-ν(-.99:.01:.99))
 
 # A strange hump
 n=35;a=zeros(n); b=sqrt(1:(n-1))/sqrt(n)*0.5
   μ=spectralmeasureU(a,b)
   ν=spectralmeasureT(a,b)
-  maximum(evaluate(μ,[-.99:.01:.99])-evaluate(ν,[-.99:.01:.99]))
+  maximum(μ(-.99:.01:.99)-ν(-.99:.01:.99))
 
 # Jacobi polynomials
 α = 1.2; β = 1.1
-  n=107;a = (β.^2-α.^2)./((2.*[0:n]+α+β).*(2.*[1:n+1]+α+β))
-  b = 2*sqrt(([1:n].*(α+[1:n]).*(β+[1:n]).*(α+β+[1:n]))./((2.*[1:n]+α+β-1).*((2.*[1:n]+α+β).^2).*(2.*[1:n]+α+β+1)))
+  n=107;a = (β.^2-α.^2)./((2.*(0:n)+α+β).*(2.*(1:n+1)+α+β))
+  b = 2*sqrt(((1:n).*(α+(1:n)).*(β+(1:n)).*(α+β+(1:n)))./((2.*(1:n)+α+β-1).*((2.*(1:n)+α+β).^2).*(2.*(1:n)+α+β+1)))
   μ=spectralmeasureU(a,b)
   ν=spectralmeasureT(a,b)
-  maximum(evaluate(μ,[-.99:.01:.99])-evaluate(ν,[-.99:.01:.99]))
+  maximum(μ(-.99:.01:.99)-ν(-.99:.01:.99))
 
 # Simplest perturbation
 k = 17
