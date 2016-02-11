f9574d16155148f610cdb6703fc5374501d81470
Marcus <marcusdavidwebb@gmail.com>
2016-02-12 09:11:24 +1100

    Fixed spectralmeasureU, made connectionCoefficientsOperator more efficient.

--- src/RatFun.jl
+++ src/RatFun.jl
@@ -1,6 +1,6 @@
-immutable RatFun{S,T}
-  p::Fun{S}
-  q::Fun{T}
+immutable RatFun{S1,T1,S2,T2}
+  p::Fun{S1,T1}
+  q::Fun{S2,T2}
 end
 
 evaluate(r::RatFun,x)=p(x)./q(x)
--- src/SpectralMeasures.jl
+++ src/SpectralMeasures.jl
@@ -6,14 +6,15 @@ import Base: +,-,*,/
 import ApproxFun:BandedOperator,ToeplitzOperator,DiracSpace, plot, IdentityOperator,
             TridiagonalOperator,addentries!,setdomain, SavedBandedOperator, resizedata!, bandinds
 
-export spectralmeasure, spectralmeasureRat, spectralmeasureU, discreteEigs, connectionCoeffsOperator
+export spectralmeasure, spectralmeasureRat, spectralmeasureU, spectralmeasureT, discreteEigs, connectionCoeffsOperator
 
-export DiscreteLaplacian
+export DiscreteLaplacian, jacobioperator
 
 include("helper.jl")
 include("ql.jl")
 include("PertToeplitz.jl")
 include("plot.jl")
+include("RatFun.jl")
 
 spectralmeasure(a,b) = spectralmeasureRat(a,b)
 
@@ -43,6 +44,34 @@ function spectralmeasureRat(a,b)
   μ
 end
 
+function spectralmeasureT(a,b)
+  # Chop the a and b down
+  a = chop!(a); b = .5+chop!(b-.5)
+  n = max(length(a),length(b)+1)
+  a = [a;zeros(n-length(a))]; b = [b;.5+zeros(n-length(b))]
+
+  # Finds C such that J*C = C*Toeplitz([0,1/2])
+  C = connectionCoeffsOperator(a,b)
+  c = Fun([C.T[1,1];C.T.negative],Taylor)
+  f = Fun(C'*(C*[1]),Ultraspherical{1}())
+
+  # Compute continuous part of measure
+  #coeffs = Fun(x->(2/pi)*(1-x^2)./abs(c(exp(im*arc,).coefficients
+  #coeffs = [coeffs[1];sqrt(2)*coeffs[2:end]] #normalised T_k polynomials
+  #μ = Fun(coeffs,JacobiWeight(.5,.5,Ultraspherical{1}()))
+
+  # Check for discrete eigenvalues
+  z = sort(real(filter!(z->abs(z)<1 && isreal(z),complexroots(c))))
+  if length(z) > 0
+    cprime = differentiate(c)
+    eigs=real(map(joukowsky,z))
+    weights = (z-1./z).^2./(z.*real(cprime(z)).*real(c(1./z)))
+    μ + Fun(weights,DiracSpace(eigs))
+  else
+    μ
+  end
+end
+
 function spectralmeasureU(a,b)
   # Chop the a and b down
   a = chop!(a); b = .5+chop!(b-.5)
@@ -55,9 +84,8 @@ function spectralmeasureU(a,b)
   f = Fun(C'*(C*[1]),Ultraspherical{1}())
 
   # Compute continuous part of measure
-  coeffs = (2/pi)*(1./f).coefficients
-  #coeffs = [coeffs[1];sqrt(2)*coeffs[2:end]] #normalised T_k polynomials
-  μ = Fun(coeffs,JacobiWeight(.5,.5,Ultraspherical{1}()))
+  coeffs = (1./f).coefficients
+  μ = Fun((2/pi)*coeffs,JacobiWeight(.5,.5,Ultraspherical{1}()))
 
   # Check for discrete eigenvalues
   z = sort(real(filter!(z->abs(z)<1 && isreal(z),complexroots(c))))
@@ -87,37 +115,41 @@ end
 # where P_k has Jacobi coeffs a,b and U_k is Chebyshev U
 function connectionCoeffsOperator(a,b)
   n = max(length(a),length(b)+1)
-  N = 2*n+1 #This is sufficient only because we go from Chebyshev U
+  N = 2*n #This is sufficient only because we go from Chebyshev U
   a = [a;zeros(N-length(a))]; b = [b;.5+zeros(N-length(b))]
-  ToeCol = zeros(N)
-  K = zeros(N,n+1)
+  ToeplitzVec = zeros(N)
+  K = zeros(N,n)
   K[1,1] = 1
   K[2,1] = -a[1]/b[1]
   K[2,2] = .5/b[1]
+  # The recurrence for the top n+1 rows depend on a and b
   for i = 3:n+1
     K[i,1] = (-a[i-1]*K[i-1,1] + .5*K[i-1,2] - b[i-2]*K[i-2,1])/b[i-1]
-    for j = 2:i-1
+    for j = 2:i-2
       K[i,j] = (.5*K[i-1,j-1] -a[i-1]*K[i-1,j] + .5*K[i-1,j+1] - b[i-2]*K[i-2,j])/b[i-1]
     end
-    K[i,i] = .5*K[i-1,i-1]/b[i-1]
+    K[i,i-1] = (.5*K[i-1,i-2] -a[i-1]*K[i-1,i-1] - b[i-2]*K[i-2,i-1])/b[i-1]
+    if i<n+1
+      K[i,i] = .5*K[i-1,i-1]/b[i-1]
+    end
   end
+  ToeplitzVec[1] = K[n,n]
+  ToeplitzVec[2] = K[n+1,n]
+  # The recurrence for rows n+2 to 2n do not depend on a and b
   for i = n+2:N
     K[i,1] = K[i-1,2] - K[i-2,1]
-    for j = 2:N+1-i
+    for j = 2:N-i
       K[i,j] = K[i-1,j-1] + K[i-1,j+1] - K[i-2,j]
     end
-    if i == n+2
-      K[n+2,n+1] = K[n+1,n] - K[n,n+1]
-    else
-      K[i,N+2-i] = K[i-1,N+1-i] + K[i-1,N+3-i] - K[i-2,N+2-i]
+    if i < N
+      K[i,N+1-i] = K[i-1,N-i] + K[i-1,N+2-i] - K[i-2,N+1-i]
     end
-    k = 2*(i-n-1)-1
-    ToeCol[k] = K[i-1,N+2-i]
-    ToeCol[k+1] = K[i,N+2-i]
+    ToeplitzVec[2*(i-n)-1] = K[i-1,N+1-i]
+    ToeplitzVec[2*(i-n)] = K[i,N+1-i]
   end
-  T = ToeplitzOperator(chop!(ToeCol[2:N]),[ToeCol[1]])
+  T = ToeplitzOperator(chop!(ToeplitzVec[2:N]),[ToeplitzVec[1]])
   for i = 1:N
-    for j = 1:min(i,N+2-i)
+    for j = 1:min(i,N+1-i)
       K[i,j]-=T[i,j]
     end
   end
